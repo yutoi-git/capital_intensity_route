49efa17779b656f3c7ce8c53d05409fb7bfe5a76
yutoi-git <yuto.i0912@gmail.com>
2023-04-03 17:55:50 +0900

    Add: モデルにバリデーションを追加

--- app/models/line.rb
+++ app/models/line.rb
@@ -1,2 +1,10 @@
 class Line < ApplicationRecord
+  self.primary_key = :line_code
+
+  belongs_to :company, foreign_key: :company_code
+  has_many :lines_stations, foreign_key: :line_code
+  has_many :stations, through: :lines_stations
+
+  validates :line_code, presence: true, uniqueness: true
+  validates :name, presence: true
 end
